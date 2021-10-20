d30c69bb9fa4aea82c66cf8a92310d0f123ebc39
Renzo Kooi <renzo.kooi@gmail.com>
2021-10-20 16:32:13 +0200

    logbox margin

--- src/JQLLog.js
+++ src/JQLLog.js
@@ -40,7 +40,7 @@ let defaultStyling = {
     overflow: `auto`,
     minHeight: `20vh`,
     maxHeight: `20vh`,
-    margin: `1rem 0 1rem 0`,
+    margin: `1rem auto`,
     padding: `0 8px 19px 8px`,
   },
   "#logBox legend": {
