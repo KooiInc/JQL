53b0fef8a5671dfab1f55a0a857618d4f70fab5d
Renzo Kooi <renzo.kooi@gmail.com>
2021-11-20 17:31:07 +0100

    Fix

--- src/Modal.js
+++ src/Modal.js
@@ -7,10 +7,11 @@ function initModal($) {
   let timer = null;
   const isTouchDevice = "ontouchstart" in document.documentElement;
   const clickOrTouch =  isTouchDevice ? "touchend" : "click";
-  $().delegate( clickOrTouch, `#closer, .between`, () => {
+  const removeIfStillActive = () => {
     $(".between").remove();
     $(".popupBox").remove();
-  } );
+  }
+  $().delegate( clickOrTouch, `#closer, .between`,  removeIfStillActive);
   const positionStuff = (theBox, closerHandle) => {
     theBox.addClass(`showAndCenter`);
 
@@ -27,7 +28,7 @@ function initModal($) {
   const endTimer = () => timer && clearTimeout(timer);
   const create = (message, omitOkBttn) => {
     endTimer();
-    closeIfActive();
+    removeIfStillActive();
     let okIcon = null;
     window.scrollTo(0, 0);
     $(`<div class="between"></div>`)
@@ -46,13 +47,13 @@ function initModal($) {
   const remove = callback => {
     endTimer();
     timer = setTimeout(() => {
-      closeIfActive();
+      removeIfStillActive();
       if (callback && callback instanceof Function) { callback(); }
     }, 300); // 300 is the fading time (ease-out)
     return timer;
   };
   const timed = (message, closeAfter = 2, callback = null, omitOkBttn = false ) => {
-    closeIfActive();
+    removeIfStillActive();
     create(message, omitOkBttn);
     const remover = callback ? () => remove(callback) : remove;
     timer = setTimeout(remover, closeAfter * 1000);
