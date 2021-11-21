d89beaeee25ce3871cecebdd760e7ae553e3c886
Renzo Kooi <renzo.kooi@gmail.com>
2021-11-21 15:30:18 +0100

    DRY

--- src/Popup.js
+++ src/Popup.js
@@ -6,7 +6,6 @@ function initModal() {
   let timer = undefined;
   let intermediateCallback;
   let canClose;
-  const stillOpen = () => alert(`A modal window is still open, make sure it is closed first!`);
   const isTouchDevice = "ontouchstart" in document.documentElement;
   let popupBox, between, closer;
   const clickOrTouch =  isTouchDevice ? "touchend" : "click";
@@ -29,6 +28,11 @@ function initModal() {
       new ResizeObserver(positionCloserHandle).observe(popupBox.first());
     }
   };
+  const stillOpen = () => {
+    alert(`A modal window is still open, make sure it is closed first!`);
+    return true;
+  }
+  const activeModalPresent = () => !canClose && popupBox.hasClass(`active`) && stillOpen() && true;
   const hideModal = () => {
     $(`#closer, .between, .popupBox`).removeClass(`active`);
     $(`body`).removeClass(`popupActive`);
@@ -54,7 +58,7 @@ function initModal() {
   };
   const endTimer = () => timer && clearTimeout(timer);
   const timed = (message, closeAfter = 2, callback = null ) => {
-    if (!canClose && popupBox.hasClass(`active`)) { return stillOpen(); }
+    if (activeModalPresent()) { return; }
     canClose = true;
     hideModal();
     create(message);
@@ -64,7 +68,7 @@ function initModal() {
   const create = (message, reallyModal = false, callback) => {
     createBoxIfNotExists();
 
-    if (!canClose && popupBox.hasClass(`active`)) { return stillOpen(); }
+    if (activeModalPresent()) { return; }
 
     intermediateCallback = callback;
     canClose = !reallyModal;
@@ -160,7 +164,7 @@ function initStyling($) {
       opacity: 0,
       width: '32px',
       height: '32px',
-      transition: 'opacity linear 0.4s 0s',
+      transition: 'opacity linear 0.7s 0s',
       background: "url('data:image/svg+xml;utf8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22iso-8859-1%22%3F%3E%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20version%3D%221.1%22%20id%3D%22Layer_1%22%20x%3D%220px%22%20y%3D%220px%22%20viewBox%3D%220%200%20128%20128%22%20style%3D%22enable-background%3Anew%200%200%20128%20128%3B%22%20xml%3Aspace%3D%22preserve%22%3E%3Crect%20x%3D%22-368%22%20y%3D%226%22%20style%3D%22display%3Anone%3Bfill%3A%23E0E0E0%3B%22%20width%3D%22866%22%20height%3D%221018%22%2F%3E%3Ccircle%20style%3D%22fill%3A%23FFFFFF%3B%22%20cx%3D%2264%22%20cy%3D%2264%22%20r%3D%2248%22%2F%3E%3Ccircle%20style%3D%22fill%3A%238CCFB9%3B%22%20cx%3D%2264%22%20cy%3D%2264%22%20r%3D%2239%22%2F%3E%3Ccircle%20style%3D%22fill%3Anone%3Bstroke%3A%23444B54%3Bstroke-width%3A6%3Bstroke-miterlimit%3A10%3B%22%20cx%3D%2264%22%20cy%3D%2264%22%20r%3D%2248%22%2F%3E%3Cpolyline%20style%3D%22fill%3Anone%3Bstroke%3A%23FFFFFF%3Bstroke-width%3A6%3Bstroke-linecap%3Around%3Bstroke-miterlimit%3A10%3B%22%20points%3D%2242%2C69%2055.55%2C81%20%20%2086%2C46%20%22%2F%3E%3C%2Fsvg%3E') no-repeat"
     },
     '.closeHandleIcon.active': {
