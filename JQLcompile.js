0719d04731335668f9e2858513acd86150e6cae3
Renzo Kooi <renzo.kooi@gmail.com>
2021-11-28 14:28:47 +0100

    Add compiler script
    Version++

---
+++ JQLcompile.js
@@ -0,0 +1,32 @@
+require('esbuild')
+  .build({
+      entryPoints: ['./src/JQueryLike.js'],
+      bundle: true,
+      outfile: './lib/JQLBundle.js',
+      sourcemap: true,
+      minify: true,
+      format: 'esm',
+      target: ['esnext'],
+      incremental: true,
+    }
+  ).then( _ => {
+    console.log(`esbuild ok`);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(`ebuild not ok: ${err.message}`);
+    process.exit(1);
+  }
+);
+
+// disabled, over to chokidar. Watch is very expensive!
+// watch: {
+//   onRebuild(error, result) {
+//     if (error) {
+//       console.error('watch build failed:', error);
+//     }
+//     else {
+//       console.log('watch build ok');
+//     }
+//   }
+// }
