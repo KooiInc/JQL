092c822d37d9018e28d337d208eae24df48959a7
Renzo Kooi <renzo.kooi@gmail.com>
2021-11-29 16:53:28 +0100

    no alert

--- src/Popup.js
+++ src/Popup.js
@@ -33,7 +33,10 @@ function popupFactory($) {
   const clickOrTouch =  "ontouchstart" in document.documentElement ? "touchend" : "click";
   $().delegate( clickOrTouch, `#closer, .between`,  remove );
   const stillOpen = () => {
-    alert(`A modal window is still open, make sure it is closed first!`);
+    endTimer();
+    modalWarner.hasClass(`active`) && modalWarner.removeClass(`active`);
+    modalWarner.addClass(`active`);
+    savedTimer = setTimeout(() => modalWarner.removeClass(`active`), 2500);
     return true;
   }
   const currentModalState = {
@@ -59,12 +62,13 @@ function popupFactory($) {
     const closer = $(`<span id="closer" class="closeHandleIcon"></span>`)
       .prop(`title`, `Click here or anywhere outside the box to close`);
     const between = $(`<div class="between"></div>`);
+    const modalWarner = popupBox.prepend(`<div id="modalWarning">Please close this box first</div>`);
     const resizeObserver = new ResizeObserver(positionCloserHandle);
     resizeObserver.observe(popupBox.first());
     resizeObserver.observe(wrappedBody.first());
-    return [popupBox, between, closer,];
+    return [popupBox, between, closer, popupBox.find$(`#modalWarning`)];
   };
-  const [popupBox, between, closer] = createElements();
+  const [popupBox, between, closer, modalWarner] = createElements();
   const hideModal = () => {
     $(`#closer, .between, .popupBox`).removeClass(`active`);
   };
@@ -113,7 +117,7 @@ function popupFactory($) {
     savedCallback = callback;
 
     if (!message.isJQL && message.constructor !== String) {
-      return createTimed($(`<b style="color:red">Modal not created: invalid input</b>`), 2);    }
+      return createTimed($(`<b style="color:red">Popup not created: invalid input</b>`), 2);    }
 
     endTimer();
     $(`.popupBox > [data-modalcontent]`).empty().append( message.isJQL ? message : $(`<div>${message}</div>`) );
@@ -247,7 +251,32 @@ function initStyling(setStyle) {
       minHeight: `1rem`,
       maxHeight: 'inherit',
       clear: 'both',
-      // padding: '8px'
+    },
+    '#modalWarning': {
+      color:`red`,
+      backgroundColor: `#FFFFF0`,
+      fontWeight: `bold`,
+      border: `1px dotted red`,
+      padding: `5px`,
+      margin: `0 auto 0.5em auto`,
+      textAlign: `center`,
+      opacity: 0,
+      maxHeight: 0,
+      height: 0,
+      position: `absolute`,
+      maxWidth: 0,
+      width: 0,
+      marginTop: `-3rem`,
+      transition: `all ease 0.4s`,
+    },
+    '#modalWarning.active': {
+      opacity: 1,
+      maxHeight: `100%`,
+      maxWidth: `100%`,
+      height: `auto`,
+      marginTop: `1rem`,
+      width: `90%`,
+      position: `sticky`,
     },
     '.popupBox p, .popupBox div': { margin: '0.4rem 0', padding: `0 6px` },
     '.popupBox p:nth-child(1), .popupBox div:nth-child(1)': { margin: '1rem 0' },
