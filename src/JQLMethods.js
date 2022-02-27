cae5e65b90c916cdbc73f340fa9cebcd384db439
Renzo Kooi <renzo.kooi@gmail.com>
2022-02-27 15:34:38 +0100

    fix val()

--- src/JQLMethods.js
+++ src/JQLMethods.js
@@ -210,9 +210,9 @@ const allMethods = {
       if (!firstElem) {
         return;
       }
-
+      
       if ([HTMLInputElement, HTMLSelectElement, HTMLTextAreaElement].includes(firstElem["constructor"])) {
-        if (value2Set || [String, Number].find(v2s => value2Set.constructor === v2s)) {
+        if (value2Set && [String, Number].find(v2s => value2Set.constructor === v2s)) {
           firstElem.value = value2Set;
         }
 
