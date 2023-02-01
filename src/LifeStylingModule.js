5a01e8f1d00a8b3de393aafd5fbe88174e8591c0
Renzo Kooi <renzo.kooi@gmail.com>
2023-02-01 23:22:40 +0100

    Update LifeStylingModule.js
    use npm link

--- src/LifeStylingModule.js
+++ src/LifeStylingModule.js
@@ -1,4 +1,4 @@
 const LifeStyleFactory = location.host === `dev.kooi` ?
   (await import("//dev.kooi/LifeCSS/index.js")).default :
-  (await import("//cdn.jsdelivr.net/gh/KooiInc/lifeCSS@latest/index.js")).default;
+  (await import("//cdn.jsdelivr.net/npm/lifecss@latest/index.js")).default;
 export default LifeStyleFactory;
