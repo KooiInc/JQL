208f6f7a7ebad75e78e4ed01c14ce96e875ca40a
Renzo Kooi <renzo.kooi@gmail.com>
2023-02-01 12:53:46 +0100

    devlink styler

--- src/LifeStylingModule.js
+++ src/LifeStylingModule.js
@@ -1,2 +1,4 @@
-import LifeStyleFactory from "//cdn.jsdelivr.net/gh/KooiInc/lifeCSS@latest/index.js";
+const LifeStyleFactory = location.host === `dev.kooi` ?
+  (await import("//dev.kooi/LifeCSS/index.js")).default :
+  (await import("//cdn.jsdelivr.net/gh/KooiInc/lifeCSS@latest/index.js")).default;
 export default LifeStyleFactory;
