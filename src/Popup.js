c0aaaf3c9c31b50e783d96b818d1cd9567c4fab5
Renzo Kooi <renzo.kooi@gmail.com>
2023-04-18 19:00:51 +0200

    popup/append fix

--- src/Popup.js
+++ src/Popup.js
@@ -4,11 +4,12 @@ export default newPopupFactory;
 function newPopupFactory($) {
   const editRule = $.createStyle(`JQLPopupCSS`);
   popupStyling.forEach( rule => editRule(rule) );
-  let [isModal, callbackOnClose, modalWarning, timeout] = [false, false, ``,];
+  let isModal, callbackOnClose, modalWarning, timeout;
   const warnTemplate = $.virtual(`<div class="popup-warn"></div>`);
-  const [closer, txtBox] = [$.virtual(`<span class="closeHandleIcon">`), $.virtual(`<div class="content">`)];
-  const popupContainer = $(`<div class="popupContainer"></div>`).append(closer, txtBox);
-  const positionCloser = () => { if (closer.hasClass(`popup-active`)) {
+  const popupContainer = $(`<div class="popupContainer">`)
+    .append( $(`<span class="closeHandleIcon">`), $(`<div class="content">`) );
+  const [closer, txtBox] = [$(`.popupContainer .closeHandleIcon`), $( `.popupContainer .content` )];
+  const positionCloser = () => { if (closer.hasClass(`popup-active`) ) {
       const {x, y, width} = txtBox.dimensions;
       closer.style({top: `${y - 12}px`, left: `${x + width - 12}px`});
     } };
@@ -45,21 +46,21 @@ function newPopupFactory($) {
   function createAndShowPupup( { content, modal, closeAfter, callback, warnMessage } ) {
     if (content) {
       clearTimeout(timeout);
-      txtBox.clear();
       setPopupZIndex(getCurrentZIndexBoundaries());
       isModal = modal ?? false;
       modalWarning = $.IS(warnMessage, String) && `${warnMessage?.trim()}`.length || warnMessage?.isJQL
         ? warnMessage : undefined;
-      txtBox.append(content.isJQL ? content.is.virtual ? content.toDOM() : content : $(`<div>${content}</div>`));
+      txtBox.clear().append(content.isJQL ? content : $(`<div>${content}</div>`));
       isModal && txtBox.append(warnTemplate.duplicate());
       popupContainer.addClass(`popup-active`);
-      callbackOnClose = callback
+      callbackOnClose = callback;
 
       if (!isModal) {
         closer.addClass(`popup-active`);
         positionCloser();
         $.IS(+closeAfter, Number) && timed(closeAfter);
       }
+
       return;
     }
     return console.error(`Popup creation needs at least some text to show`);
