b0e70850fd1665a1514e8a35728001351d0b85d9
Renzo Kooi <renzo.kooi@gmail.com>
2023-10-17 09:15:38 +0200

    re-demo

--- TestNDemo/index.js
+++ TestNDemo/index.js
@@ -27,7 +27,7 @@ $.editCssRules(...getStyleRules())
 // create container for all generated html
 const container = $.div( {
   content: $(`#logBox`).style({margin: `1rem auto`})
-    .andThen($.p({content: ``, props: {id: `JQLRoot`}} )
+    .andThen($.p( {props: {id: `JQLRoot`}} )
     .andThen($.comment(`p#JQLRoot contains all generated html`), true)),
   props: {id: `container`},
   cssClass: `MAIN`,
