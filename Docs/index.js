3fa4959e23497bb3c2a17859603c4e9cad6fa9a5
Renzo Kooi <renzo.kooi@gmail.com>
2024-11-17 17:49:24 +0100

    Line endings misery

--- Docs/index.js
+++ Docs/index.js
@@ -16,7 +16,7 @@ const setAllCodeStyling = el => {
   return !pre ? $(el).addClass(`inline`) : $(pre).addClass(`language-javascript`, `line-numbers`);
 }
 const perform = performance.now();
-document.title = isDev ? `##DEV## ${document.title}` : document.title;
+document.title = isDev ? `###DEV### ${document.title}` : document.title;
 if (isDev) {
   $(`link[rel="icon"]`).replaceWith($.LINK({href: `./devIco.png`, rel: `icon`}));
   window.jql = $;
