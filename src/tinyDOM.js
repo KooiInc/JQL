8ef9fc184dd92a2d518439a221e5cfabf741d038
Renzo Kooi <renzo.kooi@gmail.com>
2024-11-23 15:26:05 +0100

    Bump tinyDOM

--- src/tinyDOM.js
+++ src/tinyDOM.js
@@ -3,28 +3,22 @@ import {
    maybe,
  } from "../typeofAnything/typeofany.module.js";
 export default tinyDOM();
-
-const converts = {html: `innerHTML`, text: `textContent`,  class: `className`};
+const converts = { html: `innerHTML`, text: `textContent`,  class: `className` };
 
 function tinyDOM() {
   const tinyDOMProxyGetter = { get(obj, key) {
       const tag = String(key)?.toLowerCase();
       switch(true) {
         case tag in obj: return obj[tag];
-        case validateTag(tag): return (obj[tag] = tag2FN(key)) && obj[tag];
-        default: return createErrorElementFN(obj, tag, key);
-      }
-    } };
+        case validateTag(tag): return createTagFunctionProperty(obj, tag, key);
+        default: return createTagFunctionProperty(obj, tag, key, true);
+      } } };
   return new Proxy({}, tinyDOMProxyGetter);
 }
 
-function createErrorElementFN(obj, tag, key) {
-  obj[tag] = () => createElement(`b`, {style:`color:red`,text:`'${key}' is not a valid HTML-tag`});
+function createTagFunctionProperty(obj, tag, key, isError = false) {
+  Object.defineProperty(obj, tag, { get() { return isError ? _ => errorElement(key) : tag2FN(tag); } } );
   return obj[tag];
-}
-
-function validateTag(name) {
-  return isComment(name) || IS(createElement(name), {isTypes: [HTMLElement, CharacterData], notTypes: HTMLUnknownElement});
 }
 
 function processNext(root, argument, tagName) {
@@ -34,28 +28,18 @@ function processNext(root, argument, tagName) {
   });
 }
 
-function tag2FN(tagName) {
-  return (initial, ...args) => tagFN(tagName, initial, ...args);
-}
-
 function tagFN(tagName, initial, ...nested) {
   const elem = retrieveElementFromInitial(initial, tagName);
   nested?.forEach(arg => processNext(elem, arg, tagName));
   return elem;
 }
 
-function cleanupComment(initial) {
-  return IS(initial, {isTypes: Object, notTypes: Array})
-    ? initial?.text ?? initial?.textContent ?? `` : String(initial);
-}
-
 function retrieveElementFromInitial(initial, tag) {
   initial = isComment(tag) ? cleanupComment(initial) : initial;
   
   switch(true) {
-    case IS(initial, String):
-      return createElement(tag, containsHTML(initial, tag) ? {html: initial} : {text: initial});
-    case IS(initial, HTMLElement): return createElementAndAppend(tag, initial);
+    case IS(initial, String): return createElement(tag, containsHTML(initial, tag) ? {html: initial} : {text: initial});
+    case IS(initial, Node): return createElementAndAppend(tag, initial);
     default: return createElement(tag, initial);
   }
 }
@@ -70,8 +54,6 @@ function cleanupProps(props) {
   return props;
 }
 
-function isComment(tag) { return /comment/i.test(tag); }
-
 function createElementAndAppend(tag, element2Append) {
   const elem = createElement(tag);
   elem.append(element2Append);
@@ -79,15 +61,18 @@ function createElementAndAppend(tag, element2Append) {
 }
 
 function createElement(tagName, props = {}) {
-  props = IS(props, {isTypes: Object, notTypes: [Proxy, Array], defaultValue: {}});
-  const data = Object.entries(structuredClone(props?.data || {}));
+  const data = Object.entries(props?.data ?? {});
   const elem = Object.assign(
     isComment(tagName) ? new Comment() : document.createElement(tagName),
-    cleanupProps( props ) );
+    cleanupProps( IS(props, {isTypes: Object, notTypes: Array, defaultValue: {}})) );
   data.length && data.forEach(([key, value]) => elem.dataset[key] = value);
   return elem;
 }
 
-function containsHTML(str, tag) {
-  return !isComment(tag) && str.constructor === String && /<.*>|&[#|0-9|a-z]+[^;];/i.test(str);
-}
+function cleanupComment(initial) { return isRealObject(initial) ? initial?.text ?? initial?.textContent ?? `` : String(initial); }
+function errorElement(key) { return createElement(`b`, {style:`color:red`,text:`'${key}' is not a valid HTML-tag`}); }
+function containsHTML(str, tag) { return !isComment(tag) && IS(str, String) && /<.*>|&[#|0-9a-z]+[^;];/i.test(str); }
+function isRealObject(someObject) { return IS(someObject, {isTypes: Object, notTypes: [Array, null, NaN, Proxy]}) }
+function isComment(tag) { return /comment/i.test(tag); }
+function validateTag(name) { return !IS(createElement(name), HTMLUnknownElement); }
+function tag2FN(tagName) { return (initial, ...args) => tagFN(tagName, initial, ...args); }
